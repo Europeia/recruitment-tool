5b68a73bece24a685cbfb9aecc01776767338a11
Rand NS <randeuropeia@gmail.com>
2026-04-10 11:20:12 -0400

    CHORE: make me admin plz

--- cogs/base.py
+++ cogs/base.py
@@ -7,7 +7,7 @@ from components.bot import Bot
 
 def is_authorized():
     def predicate(ctx: commands.Context):
-        if ctx.author.id not in [230778695713947648, 110600636319440896]:
+        if ctx.author.id not in [230778695713947648, 110600636319440896, 134499733849899008]:
             raise commands.MissingPermissions(["Bot Administrator"])
 
         return True
